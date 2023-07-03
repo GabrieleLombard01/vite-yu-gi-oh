735b0e8f60a21ac6b5bfa706444c9398bef8a401
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-04 01:08:56 +0200

    API url add

--- src/components/pokemon/pokemonList.vue
+++ src/components/pokemon/pokemonList.vue
@@ -1,6 +1,17 @@
 <script >
+import axios from 'axios';
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?eq[type1]=Electric&sort[number]=desc';
 export default {
-
+    data() {
+        return {
+            pokemon: []
+        }
+    },
+    created() {
+        axios.get(endpoint).then(res => {
+            console.log(res.data)
+        });
+    }
 };
 </script>
 
