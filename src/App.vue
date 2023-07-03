e590bf0bb862b8b059a9183e865aada94298de99
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-04 00:37:47 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script >
+export default {
+
+};
+</script>
+
+<template>
+  <!--HEADER:-->
+  <HEADER>
+    <h1 class="text-center title_font mt-2">Pokedex</h1>
+  </HEADER>
+
+  <!--MAIN CONTENT:-->
+  <main>
+
+  </main>
+</template>
+
+<style lang="scss">
+@use '../src/assets/scss/style.scss';
+</style>
