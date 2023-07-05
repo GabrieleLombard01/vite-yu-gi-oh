5db4e8ffc553b065e27e3957111f48219e8eeda9
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-05 16:16:39 +0200

    appSelect add

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@ import axios from 'axios';
 const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons';
 
 import { store } from '../src/data/store';
-import appMain from '../src/components/appMain.vue'
+import appMain from '../src/components/appMain.vue';
+import appSelect from '../src/components/appSelect.vue';
+
 export default {
-  components: { appMain },
+  components: { appMain, appSelect },
 
   created() {
     store.isLoading = true;
@@ -24,6 +26,10 @@ export default {
   <!--HEADER:-->
   <HEADER>
     <h1 class="text-center title_font mt-2">Pokedex</h1>
+    <div class="filter">
+      <appSelect defaultLabel="All types" />
+    </div>
+
   </HEADER>
 
   <!--MAIN CONTENT:-->
---
+++ src/components/appSelect.vue
@@ -0,0 +1,26 @@
+<script >
+export default {
+    props: {
+        defaultLabel: String
+    }
+};
+</script>
+
+<template>
+    <select>
+        <option value="">{{ defaultLabel || '---' }}</option>
+        <option>Opzione 1</option>
+        <option>Opzione 2</option>
+        <option>Opzione 3</option>
+    </select>
+</template>
+
+<style>
+/*CSS reset*/
+
+/*Pseudo-classes*/
+
+/*Generics*/
+
+/*Utilties*/
+</style>
